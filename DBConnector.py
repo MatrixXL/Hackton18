9aea1f80cbf91f911c46ce77fb330da887468fcd
Yu Qiao <dennisqiao08@gmail.com>
2018-01-24 12:18:21 +0100

    do some clean work

--- DBConnector.py
+++ DBConnector.py
@@ -36,64 +36,6 @@ class DBConnector:
     def distinct(self, field):
         return self._read.distinct(field)
 
-    # def getDistinctMeasurementIDs(self):
-        # return list(self._reader.distinct('measurement_uuid'))
+    def getDistinctMeasurementIDs(self):
+        return list(self._reader.distinct('measurement_uuid'))
         
-
-    # def getTagsGroupedByAnt(self, measurement_list):
-        # groups = [[],[],[],[]]
-        # for m in measurement_list:
-            # a = list(self._reader.find({"measurement_uuid":m},{"data.AntennaPort":1, "data.EPC":1}))
-            # groups[a[0]["data"]["AntennaPort"] & 0x3].append([d["data"]["EPC"] for d in a])
-
-        # return groups
-
-    # def test(self):
-        # # pymongo.collection.Collection(self._db, "zeros", True)
-        # # coll = self._db['zeros']
-        # # coll.insert_many([{'measurement_uuid':m,  "value":0} for m in measurement_list])
-        # a = list(self._reader.aggregate([{"$match":{"data.EPC" : "c53217e28759b31e5db427319e24ae38"}},{"$project":{"measurement_uuid":1, "RSSI":"$data.RSSI"}}]))
-
-
-
-db = DBConnector()
-# db.selectReader("192.168.0.69")
-#measure = db.getDistinctMeasurementIDs()
-# g = db.getTagsGroupedByAnt(measure)
-
-# with open("outfile", 'wb') as f:
-    # pickle.dump(g, f)
-
-# db.test()
-# db.selectReader("temp_1_1_0")
-# a = list(db.find({}))
-# feature_matrix = []
-# for tag in a:
-    # feature = {}
-    # for measure in tag["obs_by"][:1000]:
-        # #feature.update({measure["measurement_uuid"]:measure["RSSI"]})
-        # feature.update({measure["measurement_uuid"]:1})
-    # feature_matrix.append(feature)
-
-# from sklearn.feature_extraction import DictVectorizer
-# v = DictVectorizer(sparse=True)
-# x = v.fit_transform(feature_matrix)
-# #x = x.transpose()
-# # import sklearn
-# # from sklearn.metrics.pairwise import pairwise_distances
-# # dist_matrix = sklearn.metrics.pairwise.pairwise_distances(x)
-# # dist_matrix = pairwise_distances(x, metric='manhattan')
-# # print(dist_matrix)
-# from sklearn.cluster import DBSCAN
-# dbscan = DBSCAN(eps=30, min_samples=2, metric='manhattan').fit(x)
-# # core_samples_mask = np.zeros_like(dbscan.labels_, dtype=bool)
-# # core_samples_mask[dbscan.core_sample_indices_] = True
-
-# labels = dbscan.labels_
-
-# result = []
-# print(len(labels))
-# for i in range(len(labels)):
-    # result.append([a[i]["_id"], labels[i]])
-
-# print(result)
--- DBInit.js
+++ DBInit.js
@@ -1,12 +1,6 @@
 conn = new Mongo();
 db = conn.getDB("RFID");
 
-//for (i = 0; i < 4; i++){
-	//db["raw_data_192.168.0.69"].aggregate([{$match:{"data.AntennaPort" : {$mod:[4, i]}}},{$project:{EPC:"$data.EPC", measurement_uuid:1}}, {$out:"temp" + i.toString()}]);
-//}
-
-//db["temp3"].mapReduce(function(){emit(this.measurement_uuid, this.EPC);}, function(key, value){return {"result":value};}, {out:"temp3_new"})
-//db["temp3"].aggregate([{$group:{_id: "$EPC", obs_by:{$push:"$measurement_uuid"}}}, {$out:"temp31"}], {allowDiskUse:true})
 for (i = 0; i < 4; i++){
 	db["raw_data_192.168.0.69"].aggregate([
 		{$sort:{measurement_uuid:1}},
@@ -17,6 +11,8 @@ for (i = 0; i < 4; i++){
 		{allowDiskUse:true});
 	db["temp"+ i.toString()].createIndex({_id: 1},{unique:true})
 }
+
+//Aufgabe 3
 
 //for (i = 0; i < 4; i++){
 	//db["raw_data_192.168.0.69"].aggregate([
